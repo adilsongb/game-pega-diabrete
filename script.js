bc1d3c8e8fd1db58793a70cea85d15421889f231
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-08-09 09:44:08 -0300

    Criado logica de cronometro e de vitoria

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let altura = window.innerHeight;
 let largura = window.innerWidth;
 let vidas = 1;
+let tempo = 10;
 
 const ajustaTamanhoTela = () => {
   altura = window.innerHeight;
@@ -8,6 +9,19 @@ const ajustaTamanhoTela = () => {
 };
 
 ajustaTamanhoTela();
+
+let cronometro = setInterval(() => {
+
+  tempo -=1;
+
+  if (tempo < 0) {
+    clearInterval(cronometro);
+    window.location.href = 'win.html';
+  } else {
+    document.getElementById('cronometro').innerHTML = tempo;
+  }
+
+}, 1000);
 
 window.onresize = ajustaTamanhoTela;
 
@@ -17,7 +31,7 @@ const posicaoRandomica = () => {
   if (document.getElementById('mosca')) {
     document.getElementById('mosca').remove();
     
-    if (vidas <= 3) {
+    if (vidas < 3) {
       document.getElementById(`v${vidas}`).src = 'imagens/coracao_vazio.png';
       vidas++;
     } else {
@@ -79,6 +93,16 @@ const ladoAleatorio = () => {
 window.onload = () => {
 
   document.body.style.height = `${altura}px`;
-  setInterval(posicaoRandomica, 1000);
+  document.getElementById('cronometro').innerHTML = tempo;
+  posicaoRandomica();
+  let criaMosca = setInterval(() => {
+    
+    if (tempo < 0) {;
+      clearInterval(criaMosca);
+    } else {
+      posicaoRandomica();
+    }
+
+  }, 2000);
 
 };
